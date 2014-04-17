09768dd2d2c4bfd545a27c8c828ccbdfd7e86387
Gary Hockin <gary@hock.in>
2014-04-17 12:15:24 +0100

    Created post entity

---
+++ module/Blog/src/Blog/Entity/Post.php
@@ -0,0 +1,127 @@
+<?php
+
+namespace Blog\Entity;
+
+
+class Post 
+{
+    protected $id;
+    protected $slug;
+    protected $writtenOn;
+    protected $title;
+    protected $preview;
+    protected $views;
+    protected $body;
+
+    /**
+     * @return mixed
+     */
+    public function getBody()
+    {
+        return $this->body;
+    }
+
+    /**
+     * @param mixed $body
+     */
+    public function setBody($body)
+    {
+        $this->body = $body;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPreview()
+    {
+        return $this->preview;
+    }
+
+    /**
+     * @param mixed $preview
+     */
+    public function setPreview($preview)
+    {
+        $this->preview = $preview;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSlug()
+    {
+        return $this->slug;
+    }
+
+    /**
+     * @param mixed $slug
+     */
+    public function setSlug($slug)
+    {
+        $this->slug = $slug;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * @param mixed $title
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getViews()
+    {
+        return $this->views;
+    }
+
+    /**
+     * @param mixed $views
+     */
+    public function setViews($views)
+    {
+        $this->views = $views;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWrittenOn()
+    {
+        return $this->writtenOn;
+    }
+
+    /**
+     * @param mixed $writtenOn
+     */
+    public function setWrittenOn($writtenOn)
+    {
+        $this->writtenOn = $writtenOn;
+    }
+} 
